3e96409ee8a65bdb1bae03f42bdd4637a58adb33
Siva Rama Krishna Pabbaraju <sivramakrishna@deloitte.com>
2025-05-09 21:33:57 +0000

    commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("zo030100010.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
